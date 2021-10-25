c5250cc9ee6599e6eec95ab733fed8736b78880b
mohan <hariharaanece3@gmail.com>
2021-10-25 22:26:42 +0530

    modified mohan file

--- mohan.java
+++ mohan.java
@@ -11,14 +11,4 @@ System.out.println("The ASCII value of " + ch1 + " is: " + asciivalue1);
 System.out.println("The ASCII value of " + ch2 + " is: " + asciivalue2);
 }
 }
-example 2
-public class PrintAsciiValueExample2
-{
-public static void main(String[] String)
-{
-int ch1 = 'a';
-int ch2 = 'b';
-System.out.println("The ASCII value of a is: "+ch1);
-System.out.println("The ASCII value of b is: "+ch2);
-}
-}
+
