5ac8894192c36aae98e31cb3ca7474337d17a1cc
mohan <hariharaanece3@gmail.com>
2021-10-25 22:04:36 +0530

    print ascii value

---
+++ mohan.java
@@ -0,0 +1,24 @@
+printing ASCII value java code
+public class PrintAsciiValue
+{
+public static void main(String[] args)
+{
+char ch1 = 'a';
+char ch2 = 'b';
+int asciivalue1 = ch1;
+int asciivalue2 = ch2;
+System.out.println("The ASCII value of " + ch1 + " is: " + asciivalue1);
+System.out.println("The ASCII value of " + ch2 + " is: " + asciivalue2);
+}
+}
+example 2
+public class PrintAsciiValueExample2
+{
+public static void main(String[] String)
+{
+int ch1 = 'a';
+int ch2 = 'b';
+System.out.println("The ASCII value of a is: "+ch1);
+System.out.println("The ASCII value of b is: "+ch2);
+}
+}
